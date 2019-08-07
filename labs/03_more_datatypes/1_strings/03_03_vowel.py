36067051fbe1ff26c20efda00e97f1b4e96ed248
Steven Recher <Buchoy@Stevens-MacBook.local>
2019-08-07 13:38:08 -0400

    03_03

--- labs/03_more_datatypes/1_strings/03_03_vowel.py
+++ labs/03_more_datatypes/1_strings/03_03_vowel.py
@@ -2,3 +2,9 @@
 Write a script prints the number of times a vowel is used in a user inputted string.
 
 '''
+
+sentence = input("Please enter a sentence: ")
+vowel = input("Please enter a vowel: ")
+
+print(sentence.count(vowel))
+
