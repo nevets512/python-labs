25166428e1b66a57c5cc4253c3ee1928b25f61ec
Steven Recher <Buchoy@Stevens-MacBook.fios-router.home>
2019-08-08 22:24:06 -0400

    03_04

--- labs/03_more_datatypes/1_strings/03_04_most_characters.py
+++ labs/03_more_datatypes/1_strings/03_04_most_characters.py
@@ -2,3 +2,15 @@
 Write a script that takes three strings from the user and prints the one with the most characters.
 
 '''
+
+sentence_1 = input("Please enter a sentence: ")
+
+sentence_2 = input("Please enter a sentence: ")
+
+sentence_3 = input("Please enter a sentence: ")
+
+list = (sentence_1, sentence_2, sentence_3)
+
+print(max(list, key = len))
+
+
